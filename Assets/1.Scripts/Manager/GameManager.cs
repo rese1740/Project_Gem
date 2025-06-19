8456925a37b2d2f3a7d3354bd2b13de6c18e9c8c
rese1740 <rese1748@naver.com>
2025-06-19 23:03:12 +0900

    [Fix] 게임 UI 표시 버그 문제 수정

--- Assets/1.Scripts/Manager/GameManager.cs
+++ Assets/1.Scripts/Manager/GameManager.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
